4a9edaa56d8369678ab451bcae28b692217cd1ae
kipic96 <kipic96gmail.com>
2017-07-25 23:23:32 +0200

    dsasad

---
+++ nauka/somethingFirst/somethingFirst/FirstClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace somethingFirst
+{
+    public class FirstClass
+    {
+        public FirstClass()
+        {
+
+        }
+        public string str = string.Empty;
+
+        public void WriteSomething(int param1, string param2)
+        {
+            Console.WriteLine("There are {0} shits and {1} up there", param1, param2);
+
+        }
+        public int autoShit { get; set; }
+         
+    }
+}
---
+++ nauka/somethingFirst/somethingFirst/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace somethingFirst
+{
+    class Program
+    {
+        static void Main()
+        {
+            //string message = "Hi man";
+            //Console.WriteLine(message);
+            int number = 20;
+            string str = "shits";
+
+            FirstClass shit = new FirstClass();
+            shit.WriteSomething(number, str);
+            
+        }
+    }
+}
